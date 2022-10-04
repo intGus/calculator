405a043ff8db64a2946e7a23e9bb2eb876ecce2b
intGus <46197948+intGus@users.noreply.github.com>
2022-10-03 20:27:29 -0400

    Add a functional Index web page and a script with a basic functioning calculator

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 //javascript file
+const add = function(a, b){
+  return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const multiply = function(a, b) {
+	return a * b;
+};
+
+const divide = function(a, b) {
+	return a / b;
+};
+
+const operate = function(a, b, operator){
+  switch(operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a,b);
+    case '*':
+      return multiply(a,b);
+    case '/':
+      return divide(a,b);
+  }
+
+}
+
+let screen ='';
+const wrapper = document.getElementById('wrapper');
+wrapper.addEventListener('click', (event) => {
+  const isButton = event.target.nodeName === 'BUTTON';
+  if (!isButton) {
+    return;
+  }
+  screen = screen + (event.target.id);
+  screenUI = document.getElementById('screen');
+  screenUI.textContent = screen
+})
