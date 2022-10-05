649136d95cbb12d54a759a3b7f673e08a0aa01ea
intGus <46197948+intGus@users.noreply.github.com>
2022-10-05 10:44:14 -0400

    Add styles to the app and fix functionality issues

--- script.js
+++ script.js
@@ -25,18 +25,52 @@ const operate = function(a, b, operator){
       return multiply(a,b);
     case '/':
       return divide(a,b);
+    default:
+      return (a + 0);
   }
 
 }
 
-let screen ='';
+let screenUI = document.getElementById('screen');
+let screen = '';
+let firstNumber = 0
+let secondNumber = 0
+let operator = ''
+
+const reset = function() {
+  screen = '';
+  firstNumber = 0
+  secondNumber = 0
+  operator = ''
+  screenUI.textContent = '0'
+};
+
 const wrapper = document.getElementById('wrapper');
 wrapper.addEventListener('click', (event) => {
   const isButton = event.target.nodeName === 'BUTTON';
   if (!isButton) {
     return;
   }
+  if (event.target.id == 'C'){
+    reset();
+    return;
+  };
+  if (event.target.getAttribute('class') == "op"){
+    if (firstNumber == 0){
+      firstNumber = Number(screen);
+      operator = (event.target.id);
+      screen = '';
+    }else{ //add second condition to verify if the operator symbolm was clicked more than once
+      secondNumber = Number(screen);
+      screen = operate(firstNumber, secondNumber, operator);
+      screenUI.textContent = screen;
+      firstNumber = 0;
+      secondNumber = 0;
+      operator = '';
+    }
+  }else{
   screen = screen + (event.target.id);
-  screenUI = document.getElementById('screen');
-  screenUI.textContent = screen
+  
+  screenUI.textContent = screen;
+  }
 })
