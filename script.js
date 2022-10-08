97fc7de2f5ad07faab84a98bdf63390730be56e7
intGus <46197948+intGus@users.noreply.github.com>
2022-10-07 22:20:40 -0400

    Fix bugs related with 0s at the beginning

--- script.js
+++ script.js
@@ -33,17 +33,28 @@ const operate = function(a, b, operator){
 
 let screenUI = document.getElementById('screen');
 let screen = '';
-let firstNumber = 0
+let firstNumber = '';
 let secondNumber = 0
 let operator = ''
+screenUI.textContent = '0'
 
 const reset = function() {
   screen = '';
-  firstNumber = 0
+  firstNumber = '';
   secondNumber = 0
   operator = ''
   screenUI.textContent = '0'
+  unselectAll()
 };
+
+const opButtons = document.getElementsByClassName('op');
+let i;
+function unselectAll() {
+  for (i = 0; i < opButtons.length; i++) {
+    opButtons[i].classList.remove("selected");
+  }
+}
+
 
 const wrapper = document.getElementById('wrapper');
 wrapper.addEventListener('click', (event) => {
@@ -55,22 +66,44 @@ wrapper.addEventListener('click', (event) => {
     reset();
     return;
   };
-  if (event.target.getAttribute('class') == "op"){
-    if (firstNumber == 0){
+
+    if (event.target.id =='back'){
+    console.log('back pressed')
+    screenUI.textContent = screenUI.textContent.slice(0, -1);
+    screen = screenUI.textContent;
+    return;
+  }
+
+  if (event.target.id == '.' && screenUI.textContent.includes('.')){
+    return;
+  };
+
+  if (event.target.classList.contains('op') ){
+    unselectAll();
+    event.target.classList.add('selected');
+    if (screen === '') {
+      console.log('screen empty')
+      operator = (event.target.id);
+      return; 
+    }
+    if (firstNumber === ''){
+      console.log('first number empty')
       firstNumber = Number(screen);
       operator = (event.target.id);
       screen = '';
-    }else{ //add second condition to verify if the operator symbolm was clicked more than once
+    }else{
       secondNumber = Number(screen);
+      console.log(firstNumber,secondNumber)
       screen = operate(firstNumber, secondNumber, operator);
-      screenUI.textContent = screen;
-      firstNumber = 0;
+      screenUI.textContent = Number(screen.toFixed(2));
+      firstNumber = '';
       secondNumber = 0;
       operator = '';
     }
   }else{
+  console.log(screenUI.textContent)
   screen = screen + (event.target.id);
-  
-  screenUI.textContent = screen;
+  console.log(screen)
+  screenUI.textContent = Number(screen);
   }
 })
